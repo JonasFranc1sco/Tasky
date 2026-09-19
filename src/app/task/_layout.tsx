b88ba6b636f8bfabae9c0698bb87ab1cb057ab46
JonasFranc1sco <jonasgouveia19@gmail.com>
2026-09-19 09:40:27 -0400

    feat: melhoria na estilização do header TaskLayout com opções explícitas

--- src/app/task/_layout.tsx
+++ src/app/task/_layout.tsx
@@ -4,7 +4,10 @@ export default function TaskLayout() {
     return (
     <Stack screenOptions={{ headerTintColor: '#0F2042', headerTitleStyle: { fontWeight: '600' } }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="[id]" options={{ title: 'Detalhes da Tarefa' }} />
+      <Stack.Screen name="[id]" options={{ title: 'Detalhes da Tarefa', 
+        headerStyle: { backgroundColor: '#FFFFFF'}, 
+        headerTintColor: '#0F2042',
+        headerTitleStyle: { color: '#0F2042', fontWeight: '600'} }} />
     </Stack>
     );
 }
